810769d9e7cb3fe676e74c008221fb42fdee12ab
hassanahmed95 <mr.hassanahmed95@gmail.com>
2019-03-20 12:45:22 +0500

    Some methods to preprocess the image, before calculating the PD in the code.  .

--- Pupilary_Distance/Pupillary_Distance.py
+++ Pupilary_Distance/Pupillary_Distance.py
@@ -17,6 +17,37 @@ face_detector_cv = cv2.dnn.readNetFromCaffe(FACE_PROTO_PATH, FACE_MODEL_PATH)
 
 
 
+#That method has been implemented in order find out that either lightening condition 
+#is sufficient or not . . . . That method also has not been used in the final version of code . . .
+
+def isLightAdequate(y):
+    NUM_BINS = 10;
+    total_pixels = y.shape[0] * y.shape[1];
+    hist = cv2.calcHist([y],[0],None,[256],[0,256])
+    hist = [i[0] for i in hist]
+    
+    dark_values = hist[0:NUM_BINS];
+    light_values = hist[-NUM_BINS:]
+
+    light = round(sum(light_values) / total_pixels * 100.0, 3)
+    dark = round(sum(dark_values) / total_pixels * 100.0, 3)
+
+    if abs(dark - light) >= 5:
+        return False, "Too dark/Too bright | "
+    else:
+        return True, ""
+
+
+#Apply some pre-processing to the image before detecting face in order to adjust the brightness of the image
+#That method is not more in use in the code, but still has been developed, just for the testing purpose . .. 
+def preProcess(bgr_image):
+    print('Applying CLAHE')
+    ycrcb = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2YCrCb)
+    adequateLight, lightMessage = isLightAdequate(ycrcb[:,:,0])
+    ycrcb[:,:,0] = CLAHE.apply(ycrcb[:,:,0])
+    return cv2.cvtColor(ycrcb, cv2.COLOR_YCrCb2BGR), adequateLight, lightMessage
+
+
 
 
 def detectFaces(image):
